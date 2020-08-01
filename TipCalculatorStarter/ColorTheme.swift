5322022279622bac2476df8eaa56a08e4bc79d5d
caye13 <Caye1300@gmail.com>
2020-07-31 21:25:58 -0700

    new proj

---
+++ TipCalculatorStarter/ColorTheme.swift
@@ -0,0 +1,45 @@
+//
+//  ColorTheme.swift
+//  TipCalculatorStarter
+//
+//  Created by Ck2 Jedi on 7/31/20.
+//  Copyright © 2020 Make School. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+struct ColorTheme {
+
+    // MARK: - Instance Vars
+
+    let isDefaultStatusBar: Bool
+    let viewControllerBackgroundColor: UIColor
+
+    let primaryColor: UIColor
+    let primaryTextColor: UIColor
+
+    let secondaryColor: UIColor
+
+    let accentColor: UIColor
+    let outputTextColor: UIColor
+    
+    // MARK: - Class Vars
+
+       static let light = ColorTheme(isDefaultStatusBar: true,
+                                     viewControllerBackgroundColor: .tcOffWhite,
+                                     primaryColor: .tcWhite,
+                                     primaryTextColor: .tcCharcoal,
+                                     secondaryColor: .tcDarkBlue,
+                                     accentColor: .tcHotPink,
+                                     outputTextColor: .tcAlmostBlack)
+
+       static let dark = ColorTheme(isDefaultStatusBar: false,
+                                    viewControllerBackgroundColor: .tcAlmostBlack,
+                                    primaryColor: .tcMediumBlack,
+                                    primaryTextColor: .tcWhite,
+                                    secondaryColor: .tcBlueBlack,
+                                    accentColor: .tcSeafoamGreen,
+                                    outputTextColor: .tcWhite)
+}
---
+++ TipCalculatorStarter/ViewController.swift
@@ -0,0 +1,151 @@
+//
+//  ViewController.swift
+//  TipCalculatorStarter
+//
+//  Created by Chase Wang on 9/19/17.
+//  Copyright © 2017 Make School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var isDefaultStatusBar = true
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return isDefaultStatusBar ? .default : .lightContent
+    }
+
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var themeSwitch: UISwitch!
+    
+    @IBOutlet weak var inputCardView: UIView!
+    @IBOutlet weak var billAmountTextField: BillAmountTextField!
+    @IBOutlet weak var tipPercentSegmentedControl: UISegmentedControl!
+    
+    @IBOutlet weak var outputCardView: UIView!
+    @IBOutlet weak var tipAmountTitleLabel: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var totalAmountTitleLabel: UILabel!
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    
+    @IBOutlet weak var resetButton: UIButton!
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupViews()
+         setTheme(isDark: false)
+
+        billAmountTextField.calculateButtonAction = {
+            self.calculate()
+        }
+    }
+    
+    @IBAction func themeToggled(_ sender: UISwitch) {
+        setTheme(isDark: sender.isOn)
+    }
+    func setTheme(isDark: Bool) {
+        let theme = isDark ? ColorTheme.dark : ColorTheme.light
+
+        view.backgroundColor = theme.viewControllerBackgroundColor
+
+        headerView.backgroundColor = theme.primaryColor
+        titleLabel.textColor = theme.primaryTextColor
+
+        inputCardView.backgroundColor = theme.secondaryColor
+
+        billAmountTextField.tintColor = theme.accentColor
+        tipPercentSegmentedControl.tintColor = theme.accentColor
+
+        outputCardView.backgroundColor = theme.primaryColor
+        outputCardView.layer.borderColor = theme.accentColor.cgColor
+
+        tipAmountTitleLabel.textColor = theme.primaryTextColor
+        totalAmountTitleLabel.textColor = theme.primaryTextColor
+
+        tipAmountLabel.textColor = theme.outputTextColor
+        totalAmountLabel.textColor = theme.outputTextColor
+
+        resetButton.backgroundColor = theme.secondaryColor
+        
+
+        isDefaultStatusBar = theme.isDefaultStatusBar
+        setNeedsStatusBarAppearanceUpdate()
+    }
+    
+    @IBAction func tipPercentChanged(_ sender: UISegmentedControl) {
+        calculate()
+    }
+    
+    func calculate() {
+           // dismiss keyboard
+           if self.billAmountTextField.isFirstResponder {
+               self.billAmountTextField.resignFirstResponder()
+           }
+
+           guard let billAmountText = self.billAmountTextField.text,
+               let billAmount = Double(billAmountText) else {
+                   clear()
+                   return
+           }
+
+           let roundedBillAmount = (100 * billAmount).rounded() / 100
+
+           let tipPercent: Double
+               switch tipPercentSegmentedControl.selectedSegmentIndex {
+                  case 0:
+                      tipPercent = 0.15
+                  case 1:
+                      tipPercent = 0.18
+                  case 2:
+                      tipPercent = 0.20
+                  default:
+                      preconditionFailure("Unexpected index.")
+                  }
+       
+           let tipAmount = roundedBillAmount * tipPercent
+           let roundedTipAmount = (100 * tipAmount).rounded() / 100
+           let totalAmount = roundedBillAmount + roundedTipAmount
+
+           // Update UI
+           self.billAmountTextField.text = String(format: "%.2f", roundedBillAmount)
+           self.tipAmountLabel.text = String(format: "%.2f", roundedTipAmount)
+           self.totalAmountLabel.text = String(format: "%.2f", totalAmount)
+       }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        clear()
+    }
+    
+    func clear() {
+        billAmountTextField.text = nil
+        tipPercentSegmentedControl.selectedSegmentIndex = 0
+        tipAmountLabel.text = "$0.00"
+        totalAmountLabel.text = "$0.00"
+    }
+    
+    func setupViews () {
+        headerView.layer.shadowOffset = CGSize(width: 0, height: 1)
+        headerView.layer.shadowOpacity = 0.05
+        headerView.layer.shadowColor = UIColor.black.cgColor
+        headerView.layer.shadowRadius = 35
+        
+
+        inputCardView.layer.cornerRadius = 8
+        inputCardView.layer.masksToBounds = true
+        
+        outputCardView.layer.cornerRadius = 8
+        outputCardView.layer.masksToBounds = true
+        
+        outputCardView.layer.borderWidth = 1
+        outputCardView.layer.borderColor = UIColor.tcHotPink.cgColor
+        
+        resetButton.layer.cornerRadius = 8
+        resetButton.layer.masksToBounds = true
+    }
+    
+}
+
